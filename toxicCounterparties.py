33d10f2abccfdf401b05c5980804fb1fecacf2e8
ManindraDeMel <manindrademel@yahoo.com.au>
2024-06-22 02:05:57 +1000

    reset

--- toxicCounterparties.py
+++ toxicCounterparties.py
@@ -1,32 +1,22 @@
-
 import numpy as np
+
+##### TODO #########################################
+### RENAME THIS FILE TO YOUR TEAM NAME #############
+### IMPLEMENT 'getMyPosition' FUNCTION #############
+### TO RUN, RUN 'eval.py' ##########################
 
 nInst = 50
 currentPos = np.zeros(nInst)
 
 
-# Define the getMyPosition function
-def getMyPosition(prices, model_path='model.pkl', scaler_path='scaler.pkl'):
-    nInst, nt = prices.shape
-    
-    # Load the model and scaler
-    with open(model_path, 'rb') as model_file:
-        model = pickle.load(model_file)
-    
-    with open(scaler_path, 'rb') as scaler_file:
-        scaler = pickle.load(scaler_file)
-    
-    features = generate_features(prices)
-    latest_features = features[:, -1, :]
-    latest_features_scaled = scaler.transform(latest_features)
-    
-    # Predict the position changes
-    position_changes = model.predict(latest_features_scaled)
-    
-    # Calculate the desired positions
-    current_prices = prices[:, -1]
-    max_position = 10000 // current_prices
-    positions = max_position * position_changes
-    
-    return positions
-
+def getMyPosition(prcSoFar):
+    global currentPos
+    (nins, nt) = prcSoFar.shape
+    if (nt < 2):
+        return np.zeros(nins)
+    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
+    lNorm = np.sqrt(lastRet.dot(lastRet))
+    lastRet /= lNorm
+    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
+    currentPos = np.array([int(x) for x in currentPos+rpos])
+    return currentPos
